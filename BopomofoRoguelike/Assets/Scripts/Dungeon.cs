5383bd72438aed44742f363415fc6c350a3aecb3
MasashiKon <111376782+MasashiKon@users.noreply.github.com>
2024-02-16 21:25:23 -0800

    add complete dungeon digging

--- BopomofoRoguelike/Assets/Scripts/Dungeon.cs
+++ BopomofoRoguelike/Assets/Scripts/Dungeon.cs
@@ -1,19 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Dungeon : MonoBehaviour
 {
-    public GameObject cell;
-    private int[][] field = { new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+    public GameObject wall;
+    public GameObject corridor;
+    public GameObject start;
+    static private int dungeonSize = 51;
+    private int[,] field = new int[dungeonSize, dungeonSize];
+    List<int[]> route = new List<int[]>();
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < field.Length; i++)
+        for (int i = 0; i < dungeonSize; i++)
         {
-            for (int j = 0; j < field[i].Length; j++)
+            for (int j = 0; j < dungeonSize; j++)
             {
-                Instantiate(cell, new Vector3(i - 5, j - 5, 0), Quaternion.identity);
+                field[i, j] = 0;
+            }
+        }
+
+        int[] startPoint = { Random.Range(0, dungeonSize - 2) + 1, Random.Range(0, dungeonSize - 2) + 1, };
+        route.Add(startPoint);
+        field[startPoint[0], startPoint[1]] = 2;
+
+        bool diggingComplete = false;
+
+        while (!diggingComplete)
+        {
+            List<int[]> directions = new List<int[]> { new int[] { -1, 0 }, new int[] { 0, 1 }, new int[] { 1, 0 }, new int[] { 0, -1 }, };
+            int progress = route.Count;
+            do
+            {
+                int index = Random.Range(0, directions.Count);
+                int[] direction = directions[index];
+                directions.RemoveAt(index);
+                int[] currentLoc = route[route.Count - 1];
+                if (currentLoc[0] + direction[0] * 2 > 0 && currentLoc[0] + direction[0] * 2 < dungeonSize - 1 && currentLoc[1] + direction[1] * 2 > 0 && currentLoc[1] + direction[1] * 2 < dungeonSize - 1 && field[currentLoc[0] + direction[0] * 2, currentLoc[1] + direction[1] * 2] == 0)
+                {
+                    field[currentLoc[0] + direction[0], currentLoc[1] + direction[1]] = 1;
+                    currentLoc = new int[] { currentLoc[0] + direction[0], currentLoc[1] + direction[1] };
+                    route.Add(currentLoc);
+                    field[currentLoc[0] + direction[0], currentLoc[1] + direction[1]] = 1;
+                    currentLoc = new int[] { currentLoc[0] + direction[0], currentLoc[1] + direction[1] };
+                    route.Add(currentLoc);
+                    break;
+                }
+                else if (directions.Count == 0)
+                {
+                    break;
+                }
+            } while (true);
+
+            if (progress == route.Count)
+            {
+                do
+                {
+                    route.RemoveAt(route.Count - 1);
+                    if (route.Count < 1) break;
+                    route.RemoveAt(route.Count - 1);
+                    if (route.Count < 1) break;
+                    int[] backTrace = { route[route.Count - 1][0], route[route.Count - 1][1] };
+                    if (backTrace[0] - 2 > 0 && field[backTrace[0] - 2, backTrace[1]] == 0)
+                    {
+                        break;
+                    }
+                    else if (backTrace[0] + 2 < dungeonSize - 1 && field[backTrace[0] + 2, backTrace[1]] == 0)
+                    {
+                        break;
+                    }
+                    else if (backTrace[1] - 2 > 0 && field[backTrace[0], backTrace[1] - 2] == 0)
+                    {
+                        break;
+                    }
+                    else if (backTrace[1] + 2 < dungeonSize - 1 && field[backTrace[0], backTrace[1] + 2] == 0)
+                    {
+                        break;
+                    }
+                } while (route.Count > 0);
+
+                if (route.Count == 0)
+                {
+                    diggingComplete = true;
+                }
+            };
+        }
+
+
+
+        for (int i = 0; i < dungeonSize; i++)
+        {
+            for (int j = 0; j < dungeonSize; j++)
+            {
+                if (field[i, j] == 0)
+                {
+                    Instantiate(wall, new Vector3(j - dungeonSize / 2, i * -1 + dungeonSize / 2, 0), Quaternion.identity);
+                }
+                else if (field[i, j] == 1)
+                {
+                    Instantiate(corridor, new Vector3(j - dungeonSize / 2, i * -1 + dungeonSize / 2, 0), Quaternion.identity);
+                }
+                else if (field[i, j] == 2)
+                {
+                    Instantiate(start, new Vector3(j - dungeonSize / 2, i * -1 + dungeonSize / 2, 0), Quaternion.identity);
+                }
+
             }
         }
     }
